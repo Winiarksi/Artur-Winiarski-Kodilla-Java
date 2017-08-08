b6942eb4796b380597399a8a0efa88812ea5c8af
“Winiarski <winiarski.a.r@gmail.com>
2017-08-08 18:24:58 +0200

    Zadanie 7.5 Obliczamy średnią, funkcyjnie V002

--- kodilla-stream/src/main/java/com/kodilla/stream/array_7_5z/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array_7_5z/ArrayOperations.java
@@ -1,6 +1,19 @@
 package com.kodilla.stream.array_7_5z;
 
-public interface ArrayOperations {
-    int[] getAverage(int[] numbers);
+import java.util.stream.IntStream;
 
+public interface ArrayOperations {
+    public static double getAverage(int[] numbers) {
+
+        IntStream.range(0, numbers.length)
+                .forEach(table -> System.out.println("INT: " + numbers[table]));
+
+        double avergardeTable = IntStream.range(0, numbers.length)
+                .map(n->numbers[n])
+                .average().getAsDouble();
+
+        System.out.println("avergardeList: " + avergardeTable);
+
+        return avergardeTable;
+    }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array_7_5z/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array_7_5z/ArrayOperationsTestSuite.java
@@ -1,31 +1,19 @@
 package com.kodilla.stream.array_7_5z;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class ArrayOperationsTestSuite {
 
     @Test
     public void testGetAverage(){
-// Test powinien przygotować tablicę z przykładowymi wartościami typu int
+        // Give
+        ArrayOperations.getAverage(new int[]{0, 1, 20, 3, 4});
 
-        ArrayOperations arrayOperations;
+        // When
+        double result = ArrayOperations.getAverage(new int[]{0, 1, 20, 3, 4});
 
-   /*         int[] tablica = new int[10];
-            for (int i = 0; i < tablica.length; i++)
-            tablica[i] = i + 1;
-*/
-
-
-        IntStream.range( 0, 10 )
-            .forEach(table -> System.out.println("INT: "+ table));
-
-        // przy pomocy strumienia IntStream oraz metody
-        // range(int startInclusive, int endExclusive)
-        // wyświetlić kolejne elementy tablicy, a następnie:
-
+        // Then
+        Assert.assertEquals(5.6, result, 0.1);
     }
 }
