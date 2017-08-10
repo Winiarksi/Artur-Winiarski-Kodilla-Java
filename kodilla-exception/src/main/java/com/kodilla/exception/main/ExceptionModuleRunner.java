f71cc587961032b444c6c1b603cbe3fe058d3bcb
“Winiarski <winiarski.a.r@gmail.com>
2017-08-10 16:40:25 +0200

    Moduł 8.2. try! catch! finally!

--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,10 +1,13 @@
 package com.kodilla.exception.main;
 
-import com.kodilla.exception.io_8_1.FileReader;
+        import com.kodilla.exception.io_8_1.FileReader;
+
+        import java.nio.file.Path;
 
 public class ExceptionModuleRunner {
     public static void main(String args[]) {
         FileReader fileReader = new FileReader();
         fileReader.readFile();
+
     }
 }
