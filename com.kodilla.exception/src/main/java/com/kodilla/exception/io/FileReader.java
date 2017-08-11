cf988c7d6b01d57dcc533a1ecaa9fe9816920d87
“Winiarski <winiarski.a.r@gmail.com>
2017-08-11 10:27:01 +0200

    Zadanie  8.2 Exception in thread main I'm gonna be here, always!

--- com.kodilla.exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ com.kodilla.exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,17 +9,19 @@ import java.util.stream.Stream;
 
 public class FileReader {
     public void readFile() {
-
         ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("file/names.txt").getFile());
-        Path path = Paths.get(file.getPath());
 
         try {
-            Stream<String> fileLines = Files.lines(path);
 
-            System.out.println(file.getPath());
-        } catch ( IOException e ) {
-            System.out.println("No i znowu się nie udało!");
+        File file = new File(classLoader.getResource("fil/names.txt").getFile());
+        //Path path = Paths.get(file.getPath().replace("%20", " "));
+        Stream<String> fileLines = Files.lines(Paths.get(file.getPath().replace("%20", " ")));
+        fileLines.forEach(System.out::println);
+
+        } catch ( IOException e) {
+            System.out.println("Oh no! Something went wrong!" + e);
+        }finally {
+            System.out.println("I'm gonna be here, always!");
         }
 
     }
