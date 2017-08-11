e6b6c23079f3db52de63724fd95152d9b1ae8d58
“Winiarski <winiarski.a.r@gmail.com>
2017-08-11 12:54:04 +0200

    Moduł 8.3. Throw i Throws - są jakieś różnice?

--- com.kodilla.exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ com.kodilla.exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,20 +8,19 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
-
         try {
-            System.out.println(classLoader.getResource("ddd.txt").getFile());
-            File file = new File(classLoader.getResource("fil/names.txt").getFile());
+
+            File file = new File(classLoader.getResource("file/ddnames.txt").getFile());
             //Path path = Paths.get(file.getPath().replace("%20", " "));
             Stream<String> fileLines = Files.lines(Paths.get(file.getPath().replace("%20", " ")));
             fileLines.forEach(System.out::println);
 
         } catch ( IOException e) {
-            System.out.println("Oh no! Something went wrong!" + e);
+            throw new FileReaderException();
         }finally {
-            System.out.println("I'm gonna be here, always!");
+            System.out.println("I am gonna be here... always!");
         }
 
     }
