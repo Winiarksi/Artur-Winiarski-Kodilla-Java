592c6511e2319bb73db9fa6262d552d868cb24a1
“Winiarski <winiarski.a.r@gmail.com>
2017-08-21 16:06:36 +0200

    Zadanie 9.4 Food2Door V001

--- com.kodilla.good.patterns_/src/main/java/challenges/Zadanie_9_4/ClassAll/MailService.java
+++ com.kodilla.good.patterns_/src/main/java/challenges/Zadanie_9_4/ClassAll/MailService.java
@@ -4,6 +4,6 @@ import challenges.Zadanie_9_4.Interface.InformationService;
 
 public class MailService implements InformationService{
     public void inForm(){
-        System.out.println("E-mail wysłany.");
+        System.out.println("E-mail wysłany. ");
     }
 }
